2229dd7cad13b2a111ac5aa67feba530fd134b17
Cam McInnes <camtmcinnes@gmail.com>
2024-09-14 07:40:41 -0700

    finished rain

--- FluidSim/rain.gd
+++ FluidSim/rain.gd
@@ -1,3 +1,4 @@
 extends Node2D
 
 var total_water_particles = 0
+var start = false
--- character_body_2d.gd
+++ character_body_2d.gd
@@ -2,13 +2,13 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -600.0
-
+const JUMP_VELOCITY = -400.0
+const gravity = 100
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += gravity * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
